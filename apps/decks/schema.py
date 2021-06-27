3e5dd02b5a687984fc4f25992ed1b8e79d9cd9fe
Faishal <faishalmanzar@gmail.com>
2021-06-27 15:40:11 +0530

    updated schema and broke into files

---
+++ apps/decks/schema.py
@@ -0,0 +1,26 @@
+from graphene_django import DjangoObjectType
+import graphene
+
+from .models import Deck
+
+
+class DeckType(DjangoObjectType):
+    class Meta:
+        model = Deck
+
+
+class CreateDeck(graphene.Mutation):
+    deck = graphene.Field(DeckType)
+
+    class Arguments:
+        title = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, title, description):
+        d = Deck(title=title, description=description)
+        d.save()
+        return CreateDeck(deck=d)
+
+# Mutation Class
+class Mutation(graphene.ObjectType):
+    create_deck = CreateDeck.Field()
